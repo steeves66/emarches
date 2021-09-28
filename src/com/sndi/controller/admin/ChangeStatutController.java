8437648e8225d736625193c150d0753ea88ca9d8
Fernand ESSOH <fernandessoh@gmail.com>
2021-09-28 10:17:50 +0000

    up du 28/09/2021

--- src/com/sndi/controller/admin/ChangeStatutController.java
+++ src/com/sndi/controller/admin/ChangeStatutController.java
@@ -1,6 +1,8 @@
 package com.sndi.controller.admin;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,21 +19,20 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.sndi.dao.WhereClause;
+import com.sndi.dao.WhereClause.Comparateur;
 import com.sndi.model.TAttributions;
 import com.sndi.model.TAvisAppelOffre;
 import com.sndi.model.TDacSpecs;
+import com.sndi.model.TDetCritAnalyseDac;
 import com.sndi.model.TDetailPlanPassation;
 import com.sndi.model.TFonction;
-import com.sndi.model.TLotAao;
-import com.sndi.model.TOperateur;
 import com.sndi.model.TSoumissions;
 import com.sndi.model.TStatut;
-import com.sndi.model.TTypeMarche;
-import com.sndi.model.VAvisAppelOffre;
 import com.sndi.model.VDacliste;
 import com.sndi.model.VLot;
 import com.sndi.model.VPpmliste;
 import com.sndi.model.VbAttributions;
+import com.sndi.model.VbStatut;
 import com.sndi.security.UserController;
 import com.sndi.service.ConstantService;
 import com.sndi.service.Iservice;
@@ -39,7 +40,7 @@ import com.sndi.utilitaires.KeyGen;
 
 @Component
 @Scope(value="session")
-@Data
+@Getter @Setter
 public class ChangeStatutController {
 	
 	Logger _logger = Logger.getLogger(ChangeStatutController.class);
@@ -58,7 +59,7 @@ public class ChangeStatutController {
 	//les listes
 	private List<VDacliste> listeDac = new ArrayList<VDacliste>();
 	private List<TFonction> listeStruc = new ArrayList<TFonction>();
-	private List<TStatut> listeStatut = new ArrayList<TStatut>();
+	private List<VbStatut> listeStatut = new ArrayList<VbStatut>();
 	private List<VbAttributions> listAttribution = new ArrayList<VbAttributions>();
 	
 	//les objets des listes
@@ -68,13 +69,10 @@ public class ChangeStatutController {
 	private TDacSpecs dac = new TDacSpecs();
 	private TAvisAppelOffre aao = new TAvisAppelOffre();
 	private TFonction struct = new TFonction();
-	private TStatut statut = new TStatut();
-	private TAttributions attrib = new TAttributions();
+	private VbStatut statut = new VbStatut();
+	private VbAttributions attrib = new VbAttributions();
 	private VbAttributions slctdAtt = new VbAttributions();
 	
-	private String OpeMat;
-	private String operateur;
-	private String structure;
 	private String strucCode;
 	private String choixOpe;
 	private String affDial;
@@ -90,8 +88,7 @@ public class ChangeStatutController {
 	private String attribStatut="";
 	
 	public void chargeData() {
-		recupActeur();
-		//chargeStructure();
+		chargeStructure();
 	}
 	
 	@PostConstruct
@@ -115,23 +112,16 @@ public class ChangeStatutController {
 	    
 	}
 	
-	public void recupActeur() {
-		OpeMat = ""+userController.getSlctd().getTOperateur().getOpeMatricule();
-		TOperateur ListeOp=(TOperateur) iservice.getObjectsByColumn("TOperateur", new ArrayList<String>(Arrays.asList("opeMatricule")),
-				new WhereClause("OPE_MATRICULE",WhereClause.Comparateur.EQ,OpeMat)).get(0);
-		operateur=ListeOp.getOpeNom();
-		structure=ListeOp.getTStructure().getStrLibelleLong();
-	}
 	
 	public void operationListe() {
 		switch(choixOpe) {
 		
 		case "ppm":
-			listeDac=iservice.getObjectsByColumn("VDacliste", new WhereClause("LBG_FON_CODE_AC",WhereClause.Comparateur.EQ,struct.getFonCod()),
+			listeDac=iservice.getObjectsByColumn("VDacliste", new WhereClause("LBG_FON_CODE_AC",WhereClause.Comparateur.EQ,""+struct.getFonCod()),
 					new WhereClause("DPP_STA_CODE",WhereClause.Comparateur.NEQ,"SDS"));
 			affDial="ppmDlg";
 			affForm="dlogPpm";
-			listeStatut=iservice.getObjectsByColumn("TStatut", new WhereClause("STA_CODE",WhereClause.Comparateur.LIKE,"S%"));
+			listeStatut=iservice.getObjectsByColumn("VbStatut", new WhereClause("STA_CODE",WhereClause.Comparateur.LIKE,"S%"));
 			ppmFrm="block";
 			dacFrm="none";
 			aaoFrm="none";
@@ -139,11 +129,11 @@ public class ChangeStatutController {
 		break;
 		case "dac":
 			listeDac=iservice.getObjectsByColumn("VDacliste", new ArrayList<String>(Arrays.asList("dacCode")),
-					new WhereClause("DAC_FON_COD_AC",WhereClause.Comparateur.EQ,struct.getFonCod()),
+					new WhereClause("DAC_FON_COD_AC",WhereClause.Comparateur.EQ,""+struct.getFonCod()),
 					new WhereClause("DAC_STA_CODE",WhereClause.Comparateur.NEQ,"SDS"));
 			affDial="dacDlg";
 			affForm="dlogDAC";
-			listeStatut=iservice.getObjectsByColumn("TStatut", new WhereClause("STA_CODE",WhereClause.Comparateur.LIKE,"D%"));
+			listeStatut=iservice.getObjectsByColumn("VbStatut", new WhereClause("STA_CODE",WhereClause.Comparateur.LIKE,"D%"));
 			ppmFrm="none";
 			dacFrm="block";
 			aaoFrm="none";
@@ -151,13 +141,11 @@ public class ChangeStatutController {
 		break;
 		case "aao":
 			listeDac=iservice.getObjectsByColumn("VDacliste", new ArrayList<String>(Arrays.asList("aaoCode")),
-					new WhereClause("AAO_FON_COD_AC",WhereClause.Comparateur.EQ,struct.getFonCod()),
+					new WhereClause("AAO_FON_COD_AC",WhereClause.Comparateur.EQ,""+struct.getFonCod()),
 					new WhereClause("DAC_STA_CODE",WhereClause.Comparateur.NEQ,"SDS"));
 			affDial="aaoDlg";
 			affForm="dlogAaao";
-			listeStatut=iservice.getObjectsByColumn("TStatut", new WhereClause("STA_CODE",WhereClause.Comparateur.LIKE,"D%"),
-					new WhereClause("DPP_STA_CODE",WhereClause.Comparateur.NOTL,"S%"),
-					new WhereClause("DPP_STA_CODE",WhereClause.Comparateur.NOTL,"D%"));
+			listeStatut=iservice.getObjectsByColumn("VbStatut", new WhereClause("STA_LIBELLE_LONG",WhereClause.Comparateur.LIKE,"%Offre%"));
 			ppmFrm="none";
 			dacFrm="none";
 			aaoFrm="block";
@@ -165,11 +153,11 @@ public class ChangeStatutController {
 		break;	
 		case "att":
 			listAttribution=iservice.getObjectsByColumn("VbAttributions", new ArrayList<String>(Arrays.asList("attNum")),
-					new WhereClause("ATT_FON_CODE_ATT",WhereClause.Comparateur.EQ,struct.getFonCod()),
-					new WhereClause("ATT_STA_CODE",WhereClause.Comparateur.NEQ,"SDS"));
+					new WhereClause("ATT_FON_CODE_ATT",WhereClause.Comparateur.EQ,""+struct.getFonCod()),
+					new WhereClause("ATT_STA_CODE",WhereClause.Comparateur.NEQ,""+"SDS"));
 			affDial="attDlg";
 			affForm="dlogAtt";
-			listeStatut=iservice.getObjectsByColumn("TStatut", new WhereClause("STA_CODE",WhereClause.Comparateur.LIKE,"AT%"));
+			listeStatut=iservice.getObjectsByColumn("VbStatut", new WhereClause("STA_CODE",WhereClause.Comparateur.LIKE,""+"AT%"));
 			ppmFrm="none";
 			dacFrm="none";
 			aaoFrm="none";
@@ -185,31 +173,30 @@ public class ChangeStatutController {
 		
 		case "ppm":
 			affValue=mixDacAao.getDppId()+" : "+mixDacAao.getDppObjet();
-			slctdPpm=(TDetailPlanPassation) iservice.getObjectsByColumn("TDetailPlanPassation", new WhereClause("DPP_ID",WhereClause.Comparateur.EQ, String.valueOf(mixDacAao.getDppId())),
-					new WhereClause("DPP_STA_CODE",WhereClause.Comparateur.NEQ,"SDS")).get(0);
+			slctdPpm=(TDetailPlanPassation) iservice.getObjectsByColumn("TDetailPlanPassation", new WhereClause("DPP_ID",WhereClause.Comparateur.EQ,""+String.valueOf(mixDacAao.getDppId()))).get(0);
 			System.out.println(slctdPpm.getDppId()+" - "+slctdPpm.getDppObjet());
 		break;
 		case "dac":
 			affValue=mixDacAao.getDacCode()+" : "+mixDacAao.getDacObjet();
-			dac=(TDacSpecs) iservice.getObjectsByColumn("TDacSpecs", new WhereClause("DAC_CODE",WhereClause.Comparateur.EQ,mixDacAao.getDacCode()),
-					new WhereClause("DAC_STA_CODE",WhereClause.Comparateur.NEQ,"SDS")).get(0);
+			List<TDacSpecs> LD  = iservice.getObjectsByColumn("TDacSpecs", new WhereClause("DAC_CODE",WhereClause.Comparateur.EQ,""+mixDacAao.getDacCode()));
+			 if(!LD.isEmpty()) dac = LD.get(0);
 			System.out.println(dac.getDacCode()+" - "+dac.getDacObjet());
 		break;
 		case "aao":
 			affValue=mixDacAao.getAaoCode()+" : "+mixDacAao.getAaoLibelle();
-			aao=(TAvisAppelOffre) iservice.getObjectsByColumn("TAvisAppelOffre", new WhereClause("AAO_CODE",WhereClause.Comparateur.EQ,mixDacAao.getAaoCode()),
-					new WhereClause("AAO_STA_CODE",WhereClause.Comparateur.NEQ,"SDS")).get(0);
+			List<TAvisAppelOffre> LA  = iservice.getObjectsByColumn("TAvisAppelOffre", new WhereClause("AAO_CODE",WhereClause.Comparateur.EQ,""+mixDacAao.getAaoCode()));
+			 if(!LA.isEmpty()) aao = LA.get(0);
 			System.out.println(aao.getAaoCode()+" - "+aao.getAaoLibelle());
 		break;	
 		case "att":
 			affValue="Attribution n"+slctdAtt.getAttNum()+" de l'appel d'offres n "+slctdAtt.getAttDofAaoCode();
-			attrib=(TAttributions) iservice.getObjectsByColumn("TAttributions", new WhereClause("ATT_NUM",WhereClause.Comparateur.EQ,slctdAtt.getAttNum().toString()),
-					new WhereClause("ATT_STA_CODE",WhereClause.Comparateur.NEQ,"SDS")).get(0);
+			List<VbAttributions> LAT  = iservice.getObjectsByColumn("VbAttributions", new WhereClause("ATT_NUM",WhereClause.Comparateur.EQ,""+slctdAtt.getAttNum().toString()));
+			 if(!LAT.isEmpty()) attrib = LAT.get(0);
 
-			TSoumissions sou = (TSoumissions) iservice.getObjectsByColumn("TSoumissions", new WhereClause("SOU_NCC",WhereClause.Comparateur.EQ,attrib.getAttSouNcc())).get(0);
+			TSoumissions sou = (TSoumissions) iservice.getObjectsByColumn("TSoumissions", new WhereClause("SOU_NCC",WhereClause.Comparateur.EQ,""+attrib.getAttSouNcc())).get(0);
 			etpre=sou.getSouNcc()+" - "+sou.getSouSigleDmp()+" - "+sou.getSouSigleSte();
 			
-			VLot avisLot = (VLot) iservice.getObjectsByColumn("VLot", new WhereClause("LAA_AAO_CODE",WhereClause.Comparateur.EQ,attrib.getAttDofAaoCode()),
+			VLot avisLot = (VLot) iservice.getObjectsByColumn("VLot", new WhereClause("LAA_AAO_CODE",WhereClause.Comparateur.EQ,""+attrib.getAttDofAaoCode()),
 					new WhereClause("LAA_ID",WhereClause.Comparateur.EQ,attrib.getAttLaaId().toString())).get(0);
 			lot="Lot n° "+avisLot.getLaaNum()+" : "+avisLot.getLaaObjet();
 			System.out.println(attrib.getAttNum()+" - "+attrib.getAttSouNcc());
@@ -220,7 +207,6 @@ public class ChangeStatutController {
 	
 	public void chargeStructure() {
 		
-		//viderDonnees();
 		if(critereRecherche.equals("")) {
 			listeStruc=(List<TFonction>)iservice.getObjectsByColumn("TFonction", new ArrayList<String>(Arrays.asList("fonCod")),
 					new WhereClause("FON_TYF_COD", WhereClause.Comparateur.EQ, "ACR"));
@@ -232,19 +218,6 @@ public class ChangeStatutController {
 		
 	}
 	
-	public void viderDonnees() {
-
-		ppm = new VPpmliste();
-		slctdPpm = new TDetailPlanPassation();
-		mixDacAao = new VDacliste();
-		dac = new TDacSpecs();
-		aao = new TAvisAppelOffre();
-		statut = new TStatut();
-		//attrib = new TAttributions();
-		//slctdAtt = new VbAttributions();
-		affValue="";
-		choixOpe="";
-	}
 	
 	public void update() {
 		System.out.println(statut.getStaCode()+" : "+statut.getStaLibelleLong());
@@ -270,8 +243,8 @@ public class ChangeStatutController {
 			iservice.updateObject(attrib);
 			break;
 		}
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO," Opération mise  jour ! ", "")); 
-		System.out.println(statut.getStaCode()+" : "+statut.getStaLibelleLong());
+		//FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO," Opération mise  jour ! ", "")); 
+		System.out.println(choixStat.getStaCode()+" : "+choixStat.getStaLibelleLong());
 	}
 
 }
